e269a6189f30ad235d7821f13fc975d7aa38f3d9
Cengiz Cinar <c.cinar@mail.de>
2024-05-27 17:43:21 +0200

    style: Update Games.jsx layout and add styles for game items

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import styles from "./styles/Games.module.css";
 
 const Games = () => {
   const [games, setGames] = useState([]);
@@ -22,9 +23,27 @@ const Games = () => {
   return (
     <>
       <h1>Spiele</h1>
-      {games.map((game) => (
-        <div key={game.id}>{game.title}</div>
-      ))}
+      <div className={styles.games_grid}>
+        {games.map((game) => (
+          <div className={styles.game_container} key={game.id}>
+            <img
+              className={styles.game_img}
+              src={game.main_game_image}
+              alt=""
+            />
+            <div className={styles.game_title_price_container}>
+              <h2 className={styles.game_title}>{game.title}</h2>
+              <span className={styles.game_price}>
+                {Number(game.price).toLocaleString("de-DE", {
+                  style: "currency",
+                  currency: "EUR",
+                })}
+              </span>
+              <div className={styles.game_platform}></div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
