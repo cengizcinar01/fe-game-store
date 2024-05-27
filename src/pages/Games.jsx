c42fe2bc783d883e62b92d97055af8a34a197194
Cengiz Cinar <c.cinar@mail.de>
2024-05-27 18:36:00 +0200

    feat: Add Xbox icon to display platform in Games component

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./styles/Games.module.css";
+import { FaXbox } from "react-icons/fa";
 
 const Games = () => {
   const [games, setGames] = useState([]);
@@ -22,7 +23,6 @@ const Games = () => {
 
   return (
     <>
-      <h1>Spiele</h1>
       <div className={styles.games_grid}>
         {games.map((game) => (
           <div className={styles.game_container} key={game.id}>
@@ -33,13 +33,17 @@ const Games = () => {
             />
             <div className={styles.game_title_price_container}>
               <h2 className={styles.game_title}>{game.title}</h2>
-              <span className={styles.game_price}>
-                {Number(game.price).toLocaleString("de-DE", {
-                  style: "currency",
-                  currency: "EUR",
-                })}
-              </span>
-              <div className={styles.game_platform}></div>
+              <div className={styles.price_platform_container}>
+                <span className={styles.game_price}>
+                  {Number(game.price).toLocaleString("de-DE", {
+                    style: "currency",
+                    currency: "EUR",
+                  })}
+                </span>
+                <div className={styles.game_platform}>
+                  <FaXbox />
+                </div>
+              </div>
             </div>
           </div>
         ))}
