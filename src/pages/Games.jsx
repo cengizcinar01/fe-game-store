f6e131467903ab32b26d1eb5c25a1036007856e3
Cengiz Cinar <c.cinar@mail.de>
2024-05-27 20:04:44 +0200

    style: Update styles for game list and filters.

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -23,33 +23,39 @@ const Games = () => {
 
   return (
     <>
-      <div className={styles.games_grid}>
-        {games.map((game) => (
-          <div className={styles.game_container} key={game.id}>
-            <img
-              className={styles.game_img}
-              src={game.main_game_image}
-              alt=""
-            />
-            <div className={styles.game_title_price_container}>
-              <h2 className={styles.game_title}>{game.title}</h2>
-              <div className={styles.price_platform_container}>
-                <span className={styles.game_price}>
-                  {Number(game.price).toLocaleString("de-DE", {
-                    style: "currency",
-                    currency: "EUR",
-                  })}
-                </span>
-                <div className={styles.game_platform}>
-                  <FaXbox />
+      <div className={styles.search_box}></div>
+      <div className={styles.left_top_filter_game_list_container}>
+        <div className={styles.filter_left}>test</div>
+        <div className={styles.game_list_filter_container}>
+          <div className={styles.filter_top}>
+            <div>test</div>
+          </div>
+          <div className={styles.games_grid}>
+            {games.map((game) => (
+              <div className={styles.game_container} key={game.id}>
+                <img className={styles.game_img} src={game.main_game_image} alt={game.title} />
+                <div className={styles.game_info}>
+                  <h2 className={styles.game_title}>{game.title}</h2>
+                  <div className={styles.game_details}>
+                    <span className={styles.game_price}>
+                      {Number(game.price).toLocaleString("de-DE", {
+                        style: "currency",
+                        currency: "EUR",
+                      })}
+                    </span>
+                    <div className={styles.game_platform}>
+                      <FaXbox />
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </>
   );
+  
 };
 
 export default Games;
