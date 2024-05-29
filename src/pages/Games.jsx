9cea4b797d65bc34ef8a13988a6deb49ce93d86a
Cengiz Cinar <c.cinar@mail.de>
2024-05-29 16:13:56 +0200

    refactor: remove unused code in Games component

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import styles from "./styles/Games.module.css";
 import GameFilter from "../components/filter/GameFilter";
@@ -8,29 +7,22 @@ import GameList from "../components/GameList";
 const Games = () => {
   const [allGames, setAllGames] = useState([]);
   const [filteredGames, setFilteredGames] = useState([]);
-  const [totalPages, setTotalPages] = useState(1);
-
-  const navigate = useNavigate();
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const currentPage = parseInt(searchParams.get("page")) || 1;
 
   useEffect(() => {
     const fetchGames = async () => {
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_API_URL}/games?page=${currentPage}`
+          `${import.meta.env.VITE_API_URL}/games`
         );
-        setAllGames(response.data.games);
-        setFilteredGames(response.data.games);
-        setTotalPages(response.data.totalPages);
+        setAllGames(response.data);
+        setFilteredGames(response.data);
       } catch (error) {
         console.error("Fehler beim Abrufen der Spiele", error);
       }
     };
 
     fetchGames();
-  }, [currentPage]);
+  }, []);
 
   const handleGenreChange = (selectedGenres) => {
     if (selectedGenres.length === 0) {
@@ -43,7 +35,6 @@ const Games = () => {
     }
   };
 
-<<<<<<< HEAD
   const handleSortChange = async (selectedSort) => {
     try {
       let response;
@@ -98,32 +89,6 @@ const Games = () => {
       />
       <GameList games={filteredGames} />
     </div>
-=======
-  const handlePageChange = (page) => {
-    navigate(`/games?page=${page}`);
-  };
-
-  return (
-    <>
-      <div className={styles.left_filter_game_list_container}>
-        <GameFilter onGenreChange={handleGenreChange} />
-        <div className={styles.game_list_pagination_container}>
-          <GameList games={filteredGames} />
-          <div className={styles.pagination}>
-            {Array.from({ length: totalPages }, (_, index) => (
-              <button
-                key={index}
-                onClick={() => handlePageChange(index + 1)}
-                className={currentPage === index + 1 ? styles.active : ""}
-              >
-                {index + 1}
-              </button>
-            ))}
-          </div>
-        </div>
-      </div>
-    </>
->>>>>>> b9070764ff1a8bbd1a7ab689b2c159a37e73d37d
   );
 };
 
