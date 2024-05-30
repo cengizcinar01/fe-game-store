639aef4fc101498863e1e2c74d7f8edc52a46285
Cengiz Cinar <c.cinar@mail.de>
2024-05-30 21:14:16 +0200

    refactor: simplify image rendering in GameDetails
    Removed the unnecessary div wrapper around the game image, and refactored the structure and styling of the game details page for better readability and cleaner code. Improved image rendering by adjusting max-width and max-height. Adjusted styling of game details for better alignment and spacing.

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -43,18 +43,16 @@ const GameDetails = () => {
         </span>
       </div>
       <div className={styles.container}>
-        <div className={styles.imageContainer}>
-          <img
-            src={game.main_game_image}
-            alt={game.title}
-            className={styles.image}
-          />
-        </div>
+        <img
+          src={game.main_game_image}
+          alt={game.title}
+          className={styles.image}
+        />
         <div className={styles.details}>
           <h1 className={styles.title}>{game.title}</h1>
           <p className={styles.genre}>{game.genre}</p>
-          <p className={styles.description}>{game.description}</p>
           <p className={styles.price}>{game.price} €</p>
+          <p className={styles.description}>{game.description}</p>
         </div>
       </div>
     </>
