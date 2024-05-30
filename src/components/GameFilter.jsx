5957faede47c94c6c500c685c71766a2aa95b2cf
Cengiz Cinar <c.cinar@mail.de>
2024-05-30 11:19:50 +0200

    refactor: update GameFilter component
    - Removed unused dependencies and code
    - Improved error handling and loading state
    - Simplified genre display logic
    - Updated game count display text

--- src/components/GameFilter.jsx
+++ src/components/GameFilter.jsx
@@ -1,32 +1,13 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "../styles/components/GameFilter.module.css";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
 import { VscSettings } from "react-icons/vsc";
 
-const GameFilter = ({ onGenreChange, onSortChange, onSearch, gameCount }) => {
-  const [genres, setGenres] = useState([]);
-  const [selectedGenres, setSelectedGenres] = useState([]);
+const GameFilter = () => {
+  const [genres, setGenres] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [isFilterOpen, setIsFilterOpen] = useState(false);
-  const searchInputRef = useRef(null);
-
-  const handleSortChange = (event) => {
-    const selectedSort = event.target.value;
-    onSortChange(selectedSort);
-
-    if (searchInputRef.current) {
-      searchInputRef.current.value = "";
-    }
-  };
-
-  const handleSearch = (event) => {
-    const searchTerm = event.target.value;
-    onSearch(searchTerm);
-
-    setSelectedGenres([]);
-    document.getElementById("filter-select").value = "title-asc";
-  };
 
   useEffect(() => {
     const fetchGenres = async () => {
@@ -35,37 +16,27 @@ const GameFilter = ({ onGenreChange, onSortChange, onSearch, gameCount }) => {
           `${import.meta.env.VITE_API_URL}/genres`
         );
         setGenres(response.data);
+        console.log(response);
+        setLoading(false);
       } catch (error) {
-        console.error("Fehler beim Abrufen der Genres", error);
+        setError(error);
+        setLoading(false);
       }
     };
 
     fetchGenres();
   }, []);
 
-  const handleGenreChange = (genre) => {
-    const updatedGenres = selectedGenres.includes(genre)
-      ? selectedGenres.filter((g) => g !== genre)
-      : [...selectedGenres, genre];
-    setSelectedGenres(updatedGenres);
-    onGenreChange(updatedGenres);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    if (searchInputRef.current) {
-      searchInputRef.current.value = "";
-    }
-  };
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   const toggleFilter = () => {
     setIsFilterOpen(!isFilterOpen);
-  };
-
-  const getGameCountText = () => {
-    if (gameCount === 0) {
-      return "Keine Spiele gefunden";
-    } else if (gameCount === 1) {
-      return "1 Spiel gefunden";
-    } else {
-      return `${gameCount} Spiele gefunden`;
-    }
   };
 
   return (
@@ -83,17 +54,11 @@ const GameFilter = ({ onGenreChange, onSortChange, onSearch, gameCount }) => {
             className={styles.search_input}
             type="text"
             placeholder="Spiel suchen..."
-            onChange={handleSearch}
-            ref={searchInputRef}
           />
         </div>
         <div className={styles.select}>
           <span className={styles.sort_text}>Sortieren:</span>
-          <select
-            id="filter-select"
-            className={styles.select_style}
-            onChange={handleSortChange}
-          >
+          <select id="filter-select" className={styles.select_style}>
             <option value="title-asc">Titel: A-Z</option>
             <option value="title-desc">Titel: Z-A</option>
             <option value="price-asc">Preis: Aufsteigend</option>
@@ -101,24 +66,13 @@ const GameFilter = ({ onGenreChange, onSortChange, onSearch, gameCount }) => {
           </select>
         </div>
         <div className={styles.genres}>
-          {genres.length > 0 ? (
-            genres.map((genre) => (
-              <label key={genre} className={styles.genre_label}>
-                <span className={styles.genre_text}>{genre}</span>
-                <input
-                  type="checkbox"
-                  checked={selectedGenres.includes(genre)}
-                  onChange={() => handleGenreChange(genre)}
-                  className={styles.genre_checkbox}
-                />
-              </label>
-            ))
-          ) : (
-            <SkeletonTheme baseColor="#202020" highlightColor="#444">
-              <Skeleton count={5} height={24.44} />
-            </SkeletonTheme>
-          )}
-          <p className={styles.game_count}>{getGameCountText()}</p>
+          {genres.map((genre) => (
+            <label key={genre} className={styles.genre_label}>
+              <span className={styles.genre_text}>{genre}</span>
+              <input type="checkbox" className={styles.genre_checkbox} />
+            </label>
+          ))}
+          <p className={styles.game_count}>X Spiele gefunden</p>
         </div>
       </div>
       <div className={styles.filter_icon_mobile} onClick={toggleFilter}>
