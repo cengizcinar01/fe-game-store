90f86effdc80186e246b0a7d84a1dc35d581ebe5
Cengiz Cinar <c.cinar@mail.de>
2024-05-30 10:49:26 +0200

    fix: Rename Navigation component to Navbar
    Renamed Navigation component to Navbar for better consistency.

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import styles from "./styles/RootLayout.module.css";
-import Navbar from "./components/Navigation";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const RootLayout = () => {
