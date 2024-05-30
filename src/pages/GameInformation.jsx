80c916a4fb4a70b9cce37ce69b663ed8ccc7aac7
Cengiz Cinar <c.cinar@mail.de>
2024-05-30 09:05:14 +0200

    feat: Fetch game information from API
    Implemented functionality to fetch game information from the API using axios and display it on the GameInformation page.

--- src/pages/GameInformation.jsx
+++ src/pages/GameInformation.jsx
@@ -1,11 +1,36 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const GameInformation = () => {
   const { gameId } = useParams();
+  const [game, setGame] = useState(null);
+
+  useEffect(() => {
+    const fetchGame = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/games/${gameId}`
+        );
+        setGame(response.data);
+      } catch (error) {
+        console.error("Fehler beim Abrufen des Spiels", error);
+      }
+    };
+
+    fetchGame();
+  }, [gameId]);
+
   return (
     <>
-      <div>GameInformation</div>
-      <p>{gameId}</p>
+      {game ? (
+        <div>
+          <h2>{game.title}</h2>
+          <p>{game.description}</p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </>
   );
 };
