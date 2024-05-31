06026f95b54607b6cc9677285d805d0050b382b1
Cengiz Cinar <c.cinar@mail.de>
2024-05-31 15:38:39 +0200

    feat: add responsive navbar menu
    Added a responsive menu to the navbar component that toggles on smaller screen sizes. Also, made minor style adjustments to accommodate the new feature.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,39 @@
 import { Link } from "react-router-dom";
-import { FaUserCircle } from "react-icons/fa";
+import { FaUserCircle, FaShoppingCart, FaBars } from "react-icons/fa";
 import { IoGameController } from "react-icons/io5";
-import { FaShoppingCart } from "react-icons/fa";
 import styles from "../styles/components/Navbar.module.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={styles.navbar_container}>
       <div className={styles.navbar_left}>
         <Link to="/" className={styles.navbar_logo}>
-          GAME STORE
           <IoGameController className={styles.navbar_game_icon} />
+          <span>GAME STORE</span>
         </Link>
-        <div className={styles.navbar_center}>
-          <Link to="/" className={styles.navbar_link}>
-            Home
-          </Link>
-          <Link to="/games" className={styles.navbar_link}>
-            Games
-          </Link>
-        </div>
       </div>
       <div className={styles.navbar_right}>
         <FaShoppingCart className={styles.navbar_cart_icon} />
         <FaUserCircle className={styles.navbar_user_icon} />
+        <FaBars className={styles.navbar_menu_icon} onClick={toggleMenu} />
       </div>
+      {menuOpen && (
+        <div className={styles.navbar_menu}>
+          <Link to="/" className={styles.navbar_link} onClick={toggleMenu}>
+            Home
+          </Link>
+          <Link to="/games" className={styles.navbar_link} onClick={toggleMenu}>
+            Games
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
