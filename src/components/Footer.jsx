0d0fb47c3f0513e87c3ba1aaaf6e66163b8f7230
Cengiz Cinar <c.cinar@mail.de>
2024-05-31 14:54:55 +0200

    feat: add payment icons to footer component
    Added payment icons (Paypal, Stripe, Visa, Apple Pay) to the footer component using react-icons library. Also, updated the styling of footer component and its child components, such as footerContent and payment_icons for better display and alignment.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import styles from "../styles/components/Footer.module.css";
+import { FaCcPaypal, FaCcStripe, FaCcVisa, FaCcApplePay } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <>
-      <footer className={styles.footer}>
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <div className={styles.payment_icons}>
+          <FaCcPaypal />
+          <FaCcStripe />
+          <FaCcVisa />
+          <FaCcApplePay />
+        </div>
         <p>&copy; 2024 Game Store. Alle Rechte vorbehalten.</p>
-      </footer>
-    </>
+      </div>
+    </footer>
   );
 };
 
