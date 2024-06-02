29eee8b007d295724bf1efccabdb4c2afa2bd7d4
Cengiz Cinar <c.cinar@mail.de>
2024-06-02 09:53:54 +0200

    feat: Enhance Dashboard user interface
    This commit introduces a new feature that improves the user interface of the Dashboard page. It includes adding new elements to the page, fetching and displaying ordered keys, and applying new styles to various elements for better visualization and user experience. This feature is intended to enhance usability and user engagement on the Dashboard page.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
-
 import { useDispatch } from "react-redux";
-
 import { fetchProtectedInfo, onLogout } from "../api/auth";
-
 import { unauthenticateUser } from "../redux/slices/authSlice";
+import axios from "axios";
+
+import styles from "../styles/pages/Dashboard.module.css";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
   const [protectedData, setProtectedData] = useState(null);
+  const [orderedKeys, setOrderedKeys] = useState([]);
 
   const logout = async () => {
     try {
@@ -31,17 +32,44 @@ const Dashboard = () => {
     }
   };
 
+  const fetchOrderedKeys = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/ordered-keys`,
+        { withCredentials: true }
+      );
+      setOrderedKeys(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     protectedInfo();
+    fetchOrderedKeys();
   }, []);
 
   return loading ? (
-    <h1>Loading...</h1>
+    <div className={styles.loadingContainer}>
+      <h1>Loading...</h1>
+    </div>
   ) : (
-    <div>
-      <h1>Dashboard</h1>
-      <h2>{protectedData}</h2>
-      <button onClick={() => logout()}>Logout</button>
+    <div className={styles.dashboardContainer}>
+      <h1 className={styles.heading}>Dashboard</h1>
+      <p className={styles.protectedData}>{protectedData}</p>
+      <button className={styles.logoutButton} onClick={() => logout()}>
+        Logout
+      </button>
+      <h2 className={styles.subheading}>Gekaufte Game Keys</h2>
+      <ul className={styles.orderedKeysList}>
+        {orderedKeys.map((key) => (
+          <li key={key.ordered_id} className={styles.orderedKey}>
+            <h3 className={styles.gameTitle}>{key.title}</h3>
+            <p className={styles.gamePrice}>Preis: {key.price} €</p>
+            <p className={styles.gameKey}>Key: {key.key}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
